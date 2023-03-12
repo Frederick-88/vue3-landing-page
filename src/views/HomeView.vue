c89363b59e0039fb1c804968a2f295aba20766e9
Chen Frederick <frederickfd88@gmail.com>
2023-03-12 22:34:13 +0700

    Removed Unused Sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Test App</h1>
+    <i class="icon-twitter"></i>
+    <div class="circle"></div>
+    <div class="circle2"></div>
+  </div>
 </template>
+
+<style lang="scss">
+.circle {
+  width: 160px;
+  height: 160px;
+  display: block;
+  box-shadow: 0px 10px 20px 0px rgba($black, 0.5), inset 0px -1px 1px 0px $white;
+  background: linear-gradient(
+    339deg,
+    rgba($white, 0.8) 0%,
+    hsl(0, 0%, 100%) 100%
+  );
+  border-radius: 100px;
+}
+
+.circle2 {
+  width: 200px;
+  height: 200px;
+  display: block;
+  border-radius: 50%;
+  border: 1px dashed rgba($black, 0.3);
+}
+</style>
