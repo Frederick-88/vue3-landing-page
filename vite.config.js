20cb15a5e0939d188391667975a084220277266c
Chen Frederick <frederickfd88@gmail.com>
2023-03-21 21:03:12 +0700

    add vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/icomoon/variables";
+          @import "./src/assets/styles/variables";
+          @import "./src/assets/styles/mixins";
+        `,
+      },
+    },
+  },
+});
