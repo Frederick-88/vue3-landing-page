fa113fb6763c865f3621ec1b55086bd36a1d1770
Chen Frederick <frederickfd88@gmail.com>
2023-03-29 21:38:07 +0700

    web performance : preload important images in first section of page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+import { preloadImage } from "../scripts/utils";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +10,13 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      beforeEnter(to, from, next) {
+        // preload important images in first page section
+        preloadImage(new URL("../assets/images/chart-desktop.jpg", import.meta.url).href);
+        preloadImage(new URL("../assets/images/chart-mobile.png", import.meta.url).href);
+
+        next();
+      },
     },
   ],
 });
---
+++ src/scripts/utils.js
@@ -0,0 +1,6 @@
+const preloadImage = (imageUrl) => {
+  const image = new Image();
+  image.src = imageUrl;
+};
+
+export { preloadImage };
